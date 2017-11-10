e51c6b9d11f2970b1eb6aa7dafc25b5da643bbc0
Bott96 <salvag96@hotmail.it>
2017-11-10 11:26:12 +0100

    File js Added

--- GestioneSegreteria/WebContent/JavaScript/FirstJSFile.js
+++ GestioneSegreteria/WebContent/JavaScript/FirstJSFile.js
@@ -10,5 +10,10 @@ function helloWorld(){
 
 function pallina(){
 	
+	var canvas = document.getElementById("myCanvas");
+	var ctx = canvas.getContext("2d");
+	ctx.fillStyle = "#FF0000";
+	ctx.fillRect(0,0,150,75);
+	
 	
 }
